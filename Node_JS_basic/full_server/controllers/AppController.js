4b88b8dcd18f71af53a8576a0147376ad8fb8ee0
tramoonyane <theteleramoonyane5@gmail.com>
2024-05-30 14:45:54 +0000

    save

--- Node_JS_basic/full_server/controllers/AppController.js
+++ Node_JS_basic/full_server/controllers/AppController.js
@@ -2,10 +2,10 @@
  * Miscellaneous route.
  */
 class AppController {
-    static getHomepage(request, response) {
-      response.status(200).send('Hello Holberton School!');
-    }
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
   }
-  
-  export default AppController;
-  module.exports = AppController;
+}
+
+export default AppController;
+module.exports = AppController;
