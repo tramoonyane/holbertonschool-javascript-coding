92160cd40d0f8bc7a805674109f1661fd636bd39
tramoonyane <theteleramoonyane5@gmail.com>
2024-05-30 13:55:20 +0000

    save

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,42 +1,36 @@
 const fs = require('fs');
 
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
 
-const countStudents = (dataPath) => {
-  if (!fs.existsSync(dataPath)) {
-    throw new Error('Cannot load the database');
-  }
-  if (!fs.statSync(dataPath).isFile()) {
-    throw new Error('Cannot load the database');
-  }
-  const fileLines = fs
-    .readFileSync(dataPath, 'utf-8')
-    .toString('utf-8')
-    .trim()
-    .split('\n');
-  const studentGroups = {};
-  const dbFieldNames = fileLines[0].split(',');
-  const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
+    const counters = {};
 
-  for (const line of fileLines.slice(1)) {
-    const studentRecord = line.split(',');
-    const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
-    const field = studentRecord[studentRecord.length - 1];
-    if (!Object.keys(studentGroups).includes(field)) {
-      studentGroups[field] = [];
+    for (let i = 1; i < lines.length; i += 1) {
+      const line = lines[i];
+      const [firstName, , , field] = line.split(',');
+
+      if (field) {
+        counters[field] = counters[field] || { count: 0, names: [] };
+        counters[field].count += 1;
+        counters[field].names.push(firstName.trim());
+      }
     }
-    const studentEntries = studentPropNames
-      .map((propName, idx) => [propName, studentPropValues[idx]]);
-    studentGroups[field].push(Object.fromEntries(studentEntries));
-  }
 
-  const totalStudents = Object
-    .values(studentGroups)
-    .reduce((pre, cur) => (pre || []).length + cur.length);
-  console.log(`Number of students: ${totalStudents}`);
-  for (const [field, group] of Object.entries(studentGroups)) {
-    const studentNames = group.map((student) => student.firstname).join(', ');
-    console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
+    console.log(`Number of students: ${lines.length - 1}`);
+    for (const field in counters) {
+      if (Object.prototype.hasOwnProperty.call(counters, field)) {
+        console.log(
+          `Number of students in ${field}: ${
+            counters[field].count
+          }. List: ${counters[field].names.join(', ')}`,
+        );
+      }
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
   }
-};
+}
 
 module.exports = countStudents;
